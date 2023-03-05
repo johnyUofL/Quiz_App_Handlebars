9cd2bc49525ee4207077d66516fdd71d9b04c66f
Your Name <you@example.com>
2023-03-05 23:36:28 +0800

    App working just needs CSS

--- app.js
+++ app.js
@@ -11,25 +11,18 @@ require('dotenv').config();
 //create express app
 const app = express();
 const port = 3000;
-
-
-
 //parsing middleware
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
 //parse application/json
 app.use(bodyParser.json());
-
 //static files
 app.use(express.static('public'));
 //serve favicon
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
 //template engine
 app.engine('hbs', exphbs({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
-
 //connection pool
 const pool = mysql.createPool({
     connectionLimit: 10,
@@ -38,16 +31,13 @@ const pool = mysql.createPool({
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
 });
-
 //connect to database
 pool.getConnection((err, connection) => {
     if (err) throw err; //not connected
     console.log('Connected as ID ' + connection.threadId);
 });
-
 const routes = require('./server/routes/user');
 app.use('/', routes);
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
